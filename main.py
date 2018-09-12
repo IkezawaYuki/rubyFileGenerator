17292296f86fda5178240def725fac19a311030d
ikezawa <ikezawa_y@worksap.co.jp>
2018-09-12 19:42:38 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import model.input_order_file
+import model.output_ruby_source
+
+
+print("hello")
+
+with open("model/template/footer.txt", "r") as f:
+    print(f.read())
+
+#　メッセージボックスを表示し、ファイルパスを受け取る
+
+
+#　オーダー定義書を読み込む
+
+
+#　ファイルにRubyを書き込む
+
+
---
+++ model/input_order_file.py
@@ -0,0 +1,6 @@
+import openpyxl
+
+
+def read_info():
+    pass
+
