d2d15c737e218c94ace78c0cc74d7f72085a83ad
ikezawa <ikezawa_y@worksap.co.jp>
2018-09-13 13:32:18 +0900

    アークより作業 読込処理をメソッドに切り出していく。

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ root.withdraw()
 fTyp = [("","*")]
 iDir = os.path.abspath(os.path.dirname(__file__))
 tkinter.messagebox.showinfo('Ruby source generator ver.1.0','選択した変換定義書のサンプルを作成します。')
-#file = tkinter.filedialog.askopenfilename(filetypes=fTyp,initialdir=iDir)
-file = "インターフェースオーダー定義書(IF051).xls"
+file = tkinter.filedialog.askopenfilename(filetypes=fTyp,initialdir=iDir)
+#file = "インターフェースオーダー定義書(IF051).xls"
 
 logger.info("Start processing...")
 logger.info("Target path is " + file)
@@ -34,12 +34,9 @@ if confirm is False:
     logger.info("システムを終了します。")
     exit(0)
 
-df = pd.read_excel(file)
-
-
 #　オーダー定義書を読みこみ、Rubyを作成。
 try:
-    str = reading.read_info(df)
+    str = reading.read_info(file)
 except:
     logger.info("読み込み時にエラー発生")
     logger.info(traceback.format_exc())
--- model/input_order_file.py
+++ model/input_order_file.py
@@ -1,7 +1,7 @@
 import openpyxl
 import tkinter.messagebox
 import logging
-
+import pandas as pd
 
 h = logging.FileHandler("log.txt")
 logger = logging.getLogger(__name__)
@@ -9,14 +9,27 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(h)
 
 
-def read_info(df):
+def read_info(filepath):
+    """エクセルの情報を取得します。Ruby作成に必要のないsheetは無視します。"""
+    file = pd.ExcelFile(filepath)
+    df_list = []
+    for sheet in file.sheet_names:
+        if sheet == "表紙" or sheet == "更新履歴" or sheet == "環境変数一覧":
+            continue
+        df_list.append((sheet, file.parse(sheet)))
+    read_sheet_info(df_list)
 
 
-    print(df)
 
-    targat_sheet = "インターフェースオーダー定義書"
+def read_sheet_info(df_list):
+    for sheetName, sheetdf in df_list:
+        print(sheetName,sheetdf)
+        for index, row in sheetdf.iterrows():
+            print(row)
 
 
+def test(df_list):
+    pass
 
 
 
