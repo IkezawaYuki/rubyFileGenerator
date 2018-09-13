e2437e24da8cf32c00ca730ac6ba27d7c77806de
IkezawaYuki <y.ikezawa93@gmail.com>
2018-09-14 00:31:13 +0900

    xlrdに乗り換え。 現状、あたいはセル単位で取り出せている。

--- main.py
+++ main.py
@@ -22,25 +22,26 @@ root = tkinter.Tk()
 root.withdraw()
 fTyp = [("","*")]
 iDir = os.path.abspath(os.path.dirname(__file__))
-tkinter.messagebox.showinfo('Ruby source generator ver.1.0','選択した変換定義書のサンプルを作成します。')
-file = tkinter.filedialog.askopenfilename(filetypes=fTyp,initialdir=iDir)
-#file = "インターフェースオーダー定義書(IF051).xls"
+tkinter.messagebox.showinfo('Ruby source generator ver.1.0',
+                            '選択した変換定義書のサンプルを作成します。')
+# file = tkinter.filedialog.askopenfilename(filetypes=fTyp,initialdir=iDir)
+file = "インターフェースオーダー定義書(IF051).xls"
 
 logger.info("Start processing...")
 logger.info("Target path is " + file)
 confirm = tkinter.messagebox.askokcancel('Sample file generator ver1.0',
-                                             '以下のファイルのサンプルデータを作成します。\n\n' + file)
+                                        '以下のファイルのサンプルデータを作成します。\n\n' + file)
 if confirm is False:
     logger.info("システムを終了します。")
     exit(0)
 
-#　オーダー定義書を読みこみ、Rubyを作成。
-try:
-    str = reading.read_info(file)
-except:
-    logger.info("読み込み時にエラー発生")
-    logger.info(traceback.format_exc())
+# オーダー定義書を読みこみ、Rubyを作成。
 
-#　ファイルにRubyを出力する
+str = reading.read_info(file)
+
+logger.info("読み込み時にエラー発生")
+logger.info(traceback.format_exc())
+
+# ファイルにRubyを出力する
 
 
--- model/input_order_file.py
+++ model/input_order_file.py
@@ -2,6 +2,7 @@ import openpyxl
 import tkinter.messagebox
 import logging
 import pandas as pd
+import xlrd
 
 h = logging.FileHandler("log.txt")
 logger = logging.getLogger(__name__)
@@ -11,22 +12,28 @@ logger.addHandler(h)
 
 def read_info(filepath):
     """エクセルの情報を取得します。Ruby作成に必要のないsheetは無視します。"""
-    file = pd.ExcelFile(filepath)
-    df_list = []
-    for sheet in file.sheet_names:
-        if sheet == "表紙" or sheet == "更新履歴" or sheet == "環境変数一覧":
+    file = xlrd.open_workbook(filepath)
+    for i in range(file.nsheets):
+        sheet = file.sheet_by_index(i)
+        print(sheet.name)
+        if sheet.name == "更新履歴" or sheet.name == "表紙" or sheet.name == "環境変数一覧":
             continue
-        df_list.append((sheet, file.parse(sheet)))
-    read_sheet_info(df_list)
+        for row_index in range(sheet.nrows):
+            row = sheet.row(row_index)
+            for cell in row:
+                # 全ての情報がセル単位で取り出すことに成功中
+                print(cell.value)
+            print(row[8])
 
 
 
-def read_sheet_info(df_list):
-    for sheetName, sheetdf in df_list:
-        print(sheetName,sheetdf)
-        for index, row in sheetdf.iterrows():
-            print(row)
+def read_sheet_info(file, target):
+    for sheet in target:
+        input_sheet_df = file.parse(sheet)
 
+
+
+    return str
 
 def test(df_list):
     pass
